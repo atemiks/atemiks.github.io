5c142140a70d41065ff60ed2c1a6226814f37aa9
atemiks <atemiks@gmail.com>
2017-09-02 13:37:00 +0300

    domarastut

---
+++ domarastut/js/main.js
@@ -0,0 +1,70 @@
+$(document).ready(function () {
+    'use strict'
+
+    // animation effect
+    jQuery('.post').addClass("unshow").viewportChecker({
+        classToAdd: 'show animated fadeInUp',
+        offset: 100
+    });
+
+    // header
+    $('.header-toggle').on('click', function(e) {
+		e.preventDefault();
+		$(this).toggleClass('active');
+		$('.header-nav').toggleClass('active');
+	});
+
+	/*scroll id*/
+	$('.header-nav a').click(function() {
+	    var target = $(this).attr('href');
+	    $('html, body').animate({
+	        scrollTop: $(target).offset().top
+	    }, 1000);
+	    $(".header-toggle").click();
+	    return false;
+	});
+
+    
+
+    $('.slider-catalog').slick({
+    	dots: true,
+    	adaptiveHeight: true,
+    	fade: true,
+  		cssEase: 'linear'
+	});
+
+	$('.slider-news').slick({
+    	dots: false,
+    	adaptiveHeight: true,
+    	fade: true,
+  		cssEase: 'linear'
+	});
+
+    // new js
+    $( ".content-aside-toggle " ).on( "click", function() {
+        $('.content-aside-inner').slideToggle();
+    });
+
+
+    $(".select2").select2({
+      minimumResultsForSearch: Infinity
+    });
+    $(".select-subway").select2({
+      placeholder: "Выберите или найдите.."
+    });
+
+    $('.nstSlider').nstSlider({
+        "rounding": {
+            "100": "1000",
+            "1000": "10000",
+            "10000": "100000"
+        },
+        "left_grip_selector": ".leftGrip",
+        "right_grip_selector": ".rightGrip",
+        "value_bar_selector": ".bar",
+        "value_changed_callback": function(cause, leftValue, rightValue) {
+            $(this).parent().find('.leftLabel').text(leftValue);
+            $(this).parent().find('.rightLabel').text(rightValue);
+        }
+    });
+});
