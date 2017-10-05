ef65f10f0c26a569c674a2f3e3062e9c186129be
atemiks <atemiks@gmail.com>
2017-10-05 07:53:32 +0300

    version 1.1

--- shipping/js/main.js
+++ shipping/js/main.js
@@ -1,7 +1,33 @@
 $(document).ready(function() {
     'use strict'
 
-    $("body").niceScroll(); 
+    function getInternetExplorerVersion() {
+        var rv = -1;
+        if (navigator.appName == 'Microsoft Internet Explorer') {
+            var ua = navigator.userAgent;
+            var re = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+            if (re.exec(ua) != null)
+                rv = parseFloat(RegExp.$1);
+        } else if (navigator.appName == 'Netscape') {
+            var ua = navigator.userAgent;
+            var re = new RegExp("Trident/.*rv:([0-9]{1,}[\.0-9]{0,})");
+            if (re.exec(ua) != null)
+                rv = parseFloat(RegExp.$1);
+        }
+        return rv;
+    }
+
+    if(getInternetExplorerVersion()!==-1){
+      //Значит это IE
+    }
+    else
+        $("body").niceScroll(); 
+
+
+
+    
+
+
 
     // header
     var 
