f9b2575224193e6f9c061f7f99b98456fa32e2df
atemiks <atemiks@gmail.com>
2017-11-28 00:17:31 +0200

    atemiks

--- atemiks/js/main.js
+++ atemiks/js/main.js
@@ -26,6 +26,27 @@ $(document).ready(function() {
             }
         });
 
+    var offset = 300,
+        //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+        //duration of the top scrolling animation (in ms)
+        scrollTopDuration = 700,
+        //grab the "back to top" link
+        $headerFixed = $('.header-box');
+
+    //hide or show the "back to top" link
+    $(window).scroll(function(){
+        ( $(this).scrollTop() > offset ) ? $headerFixed.addClass('header-fixed') : $headerFixed.removeClass('header-fixed');
+    });
+
+    //smooth scroll to top
+    // $headerFixed.on('click', function(event){
+    //     event.preventDefault();
+    //     $('body,html').animate({
+    //         scrollTop: 0 ,
+    //         }, scrollTopDuration
+    //     );
+    // });
+
 
     // header
  //    $(".header-toggle, .mobile-navigation-close").on( "click", function() {
