e19d713b049a285573155194d41f4f00b8445fb9
atemiks <atemiks@gmail.com>
2018-02-02 12:42:51 +0200

    DWhouse

--- DWhouse/js/main.js
+++ DWhouse/js/main.js
@@ -1,38 +1,51 @@
 $( document ).ready(function() {
 
-	$('.select-custom').select2({
-		 minimumResultsForSearch: Infinity
-	});
+	// custom select
 
-	// main search
-	$(".select-custom-style").select2({
-		minimumResultsForSearch: Infinity,
-	    placeholder: "Styles Collections",
-	    allowClear: true
-	});
+	$('.select-custom').each(function(){
+	    var $this = $(this), numberOfOptions = $(this).children('option').length;
+	  
+	    $this.addClass('select-hidden'); 
+	    $this.wrap('<div class="select"></div>');
+	    $this.after('<div class="select-styled"></div>');
 
-	$(".select-custom-beds").select2({
-		minimumResultsForSearch: Infinity,
-	    placeholder: "Beds",
-	    allowClear: true
-	});
+	    var $styledSelect = $this.next('div.select-styled');
+	    $styledSelect.text($this.children('option').eq(0).text());
+	  
+	    var $list = $('<ul />', {
+	        'class': 'select-options'
+	    }).insertAfter($styledSelect);
+	  
+	    for (var i = 0; i < numberOfOptions; i++) {
+	        $('<li />', {
+	            text: $this.children('option').eq(i).text(),
+	            rel: $this.children('option').eq(i).val()
+	        }).appendTo($list);
+	    }
+	  
+	    var $listItems = $list.children('li');
+	  
+	    $styledSelect.click(function(e) {
+	        e.stopPropagation();
+	        $('div.select-styled.active').not(this).each(function(){
+	            $(this).removeClass('active').next('ul.select-options').hide();
+	        });
+	        $(this).toggleClass('active').next('ul.select-options').toggle();
+	    });
+	  
+	    $listItems.click(function(e) {
+	        e.stopPropagation();
+	        $styledSelect.text($(this).text()).removeClass('active');
+	        $this.val($(this).attr('rel'));
+	        $list.hide();
+	        //console.log($this.val());
+	    });
+	  
+	    $(document).click(function() {
+	        $styledSelect.removeClass('active');
+	        $list.hide();
+	    });
 
-	$(".select-custom-baths").select2({
-		minimumResultsForSearch: Infinity,
-	    placeholder: "Baths",
-	    allowClear: true
-	});
-
-	$(".select-custom-stories").select2({
-		minimumResultsForSearch: Infinity,
-	    placeholder: "Stories",
-	    allowClear: true
-	});
-
-	$(".select-custom-garages").select2({
-		minimumResultsForSearch: Infinity,
-	    placeholder: "Garages",
-	    allowClear: true
 	});
 
 
