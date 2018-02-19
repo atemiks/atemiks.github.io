002fc20fd19e8192c4b0e00da7438ecf2c89d4e6
atemiks <atemiks@gmail.com>
2018-02-19 09:46:11 +0200

    DWhouse

--- DWhouse/js/main.js
+++ DWhouse/js/main.js
@@ -79,7 +79,7 @@ $( document ).ready(function() {
 		slidesToScroll: 1,
 		dots: true,
 	 	autoplay: true,
-  		autoplaySpeed: 3000,
+  		autoplaySpeed: 5000,
   		pauseOnFocus: false,
   		pauseOnHover: false
 
