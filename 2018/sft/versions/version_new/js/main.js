18e6d9aa6b552be2fe32aab424a0d71f6fe77af9
atemiks <atemiks@gmail.com>
2018-04-13 12:32:28 +0300

    sft

--- 2018/sft/versions/version_new/js/main.js
+++ 2018/sft/versions/version_new/js/main.js
@@ -171,10 +171,10 @@ $( document ).ready(function() {
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
-        zoom: 11,
+        zoom: 14,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(55.76, 37.61455332),
+        center: new google.maps.LatLng(55.7897495, 37.5023705),
 
         // How you would like to style the map. 
         // This is where you would paste any style found on Snazzy Maps.
@@ -190,7 +190,7 @@ $( document ).ready(function() {
 
     // Let's also add a marker while we're at it
     var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(55.78363614, 37.61455332),
+        position: new google.maps.LatLng(55.7897495, 37.5023705),
         map: map,
         // icon: './img/icons/icon-location.png',
         icon: './img/icons/icon-location-primary.svg',
