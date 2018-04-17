ab6d05c699ec1d8372997d31e713cc27f9b673a4
atemiks <atemiks@gmail.com>
2018-04-17 19:31:57 +0300

    yourkey

--- 2018/yourkey/js/main.js
+++ 2018/yourkey/js/main.js
@@ -17,14 +17,14 @@ $( document ).ready(function() {
         $('.offcanvas').toggleClass('offcanvas-active');
     });
 
-    $('.offcanvas').click(function(e) {
-        console.log(this);
-        var offcanvasMenu = $('.offcanvas-inner, .header-toggle');
-        if ( !offcanvasMenu.is(e.target) && offcanvasMenu.has(e.target).length === 0 ) {
-            $('html, body').toggleClass('offcanvas-open');
-            $('.offcanvas').toggleClass('offcanvas-active');
-        }
-    });
+    // $('.offcanvas').click(function(e) {
+    //     console.log(this);
+    //     var offcanvasMenu = $('.offcanvas-inner, .header-toggle');
+    //     if ( !offcanvasMenu.is(e.target) && offcanvasMenu.has(e.target).length === 0 ) {
+    //         $('html, body').toggleClass('offcanvas-open');
+    //         $('.offcanvas').toggleClass('offcanvas-active');
+    //     }
+    // });
 
 
 
