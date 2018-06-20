6b0acc6083a58590d2ed0c7507b72badf8f45825
atemiks <atemiks@gmail.com>
2018-06-20 13:48:35 +0300

    think

---
+++ 2018/think/js/main.js
@@ -0,0 +1,50 @@
+$( document ).ready(function() {
+
+    // smooth scroll
+    // Select all links with hashes
+    $('a[href*="#"]')
+      // Remove links that don't actually link to anything
+      .not('[href="#"]')
+      .not('[href="#0"]')
+      .click(function(event) {
+        // On-page links
+        if (
+          location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+          && 
+          location.hostname == this.hostname
+        ) {
+          // Figure out element to scroll to
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          // Does a scroll target exist?
+          if (target.length) {
+            // Only prevent default if animation is actually gonna happen
+            event.preventDefault();
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 300);
+          }
+        }
+      });
+
+
+    // float labels
+    //This code is a bit rudimentary.
+    //This is more of a proof of concept than code for production.
+    //The only thing it needs to do, however, is to check if the field has any value. The rest is done with CSS
+
+    $(document).ready(function(){
+      function updateText(event){
+        var input=$(this);
+        setTimeout(function(){
+          var val=input.val();
+          if(val!="")
+            input.parent().addClass("floating-placeholder-float");
+          else
+            input.parent().removeClass("floating-placeholder-float");
+        },1)
+      }
+      $(".floating-placeholder input").keydown(updateText);
+      $(".floating-placeholder input").change(updateText);
+    });
+});
