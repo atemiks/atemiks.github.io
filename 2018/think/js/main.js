c24e358a732d86aaf5f701bc523996ccb1eac405
atemiks <atemiks@gmail.com>
2018-06-20 16:16:38 +0300

    think

--- 2018/think/js/main.js
+++ 2018/think/js/main.js
@@ -28,6 +28,25 @@ $( document ).ready(function() {
       });
 
 
+    // sticky header
+    window.onscroll = function() {myFunction()};
+
+    // Get the header
+    var header = document.getElementById("header-navbar");
+
+    // Get the offset position of the navbar
+    var sticky = 200;
+
+    // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+    function myFunction() {
+      if (window.pageYOffset >= sticky) {
+        header.classList.add("navbar-scroll");
+      } else {
+        header.classList.remove("navbar-scroll");
+      }
+    }
+
+
     // float labels
     //This code is a bit rudimentary.
     //This is more of a proof of concept than code for production.
