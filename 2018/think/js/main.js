f4cc8960e41e54d9a16c71a4a4b3e47dc63e9df4
atemiks <atemiks@gmail.com>
2018-07-09 22:14:06 +0300

    think

--- 2018/think/js/main.js
+++ 2018/think/js/main.js
@@ -85,18 +85,34 @@ $( document ).ready(function() {
 
     // check ie
     function msieversion() {
-
         var ua = window.navigator.userAgent;
-        var msie = ua.indexOf("MSIE ");
 
-        if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./)) {
-            $('.blur').addClass('ie-blur');
-            alert(parseInt(ua.substring(msie + 5, ua.indexOf(".", msie))));
+        var msie = ua.indexOf('MSIE ');
+        if (msie > 0) {
+            // IE 10 or older => return version number
+            return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
         }
 
+        var trident = ua.indexOf('Trident/');
+        if (trident > 0) {
+            // IE 11 => return version number
+            var rv = ua.indexOf('rv:');
+            return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+        }
+
+        // var edge = ua.indexOf('Edge/');
+        // if (edge > 0) {
+        //    // Edge (IE 12+) => return version number
+        //    return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+        // }
+
+        // other browser
         return false;
     }
-    msieversion();
+    if (msieversion() ) {
+        $('.blur').addClass('ie-blur');
+        alert('ie');
+    }
     
 
     // section hero
