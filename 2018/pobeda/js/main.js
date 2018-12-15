8b9f7fea3139a2c006a6296d3739e537e1a7ea61
atemiks <atemiks@gmail.com>
2018-12-15 13:23:56 +0200

    pobeda

--- 2018/pobeda/js/main.js
+++ 2018/pobeda/js/main.js
@@ -1,18 +1,34 @@
 $( document ).ready(function() {
-	// animation
-	setTimeout(function(){
-	  AOS.init();
-	}, 100);
-	
-	// phone mask
-	$("input[type='tel']").mask("+7 (999) 999-99-99");
+    // animation
+    setTimeout(function(){
+      AOS.init();
+    }, 100);
 
-	// header
-	$( ".header-offcanvas-toggler, .offcanvas-close" ).on( "click", function() {
-		$('.offcanvas').toggleClass('visible');
-	});
+    // phone mask
+    $("input[type='tel']").mask("+7 (999) 999-99-99");
+
+    // header
+    $( ".header-offcanvas-toggler, .offcanvas-close" ).on( "click", function() {
+        $('.offcanvas').toggleClass('visible');
+    });
 
     // calculator
+    $.fn.setCursorPosition = function (pos) {
+        this.each(function (index, elem) {
+            if (elem.setSelectionRange) {
+                elem.setSelectionRange(pos, pos);
+            } else if (elem.createTextRange) {
+                var range = elem.createTextRange();
+                range.collapse(true);
+                range.moveEnd('character', pos);
+                range.moveStart('character', pos);
+                range.select();
+            }
+        });
+        return this;
+    };
+
+    var coeff = 4.5;
     var $rangeSumSlider = $('#range-sum-slider');
     var $rangeSumImport = $('#range-sum-input');
 
@@ -21,10 +37,10 @@ $( document ).ready(function() {
 
     var getSum = $('#range-sum-input').val();
     var getTime = $('#range-time-input').val();
-    var resultSum = getSum * .9;
-    var resultCalc =  getSum / getTime;
-    $('.result-box .result-sum').text(resultSum.toFixed(2));
-    $('.result-box .result').text(resultCalc.toFixed(2));
+    var resultSum = getSum / 100 * coeff;
+    var resultCalc =  getSum / 100 * (coeff / 30);
+    $('.result-box .result-sum span').text(resultSum.toFixed(0)).number(true, 0);
+    $('.result-box .result span').text(resultCalc.toFixed(0)).number(true, 0);
 
     // range sum
     $rangeSumSlider
@@ -37,46 +53,56 @@ $( document ).ready(function() {
             getTime = $('#range-time-input').val();
 
             if(getSum != 0 && getTime != 0 ) {
-            	resultSum = getSum * .9;
-                resultCalc = getSum / getTime;
-                $('.result-box .result-sum').text(resultSum.toFixed(2));
-                $('.result-box .result').text(resultCalc.toFixed(2));
+                resultSum = getSum / 100 * coeff;
+                resultCalc = getSum / 100 * (coeff / 30);
+                $('.result-box .result-sum span').text(resultSum.toFixed(0)).number(true, 0);
+                $('.result-box .result span').text(resultCalc.toFixed(0)).number(true, 0);
             }
             else {
-            	$('.result-box .result-sum').text('0');
-                $('.result-box .result').text('0');
+                $('.result-box .result-sum span').text('0');
+                $('.result-box .result span').text('0');
             }
         });
 
     $rangeSumImport.on('input', function() {
-        if (this.value == "") {
-            this.value = 0;
-            $rangeSumSlider.val(this.value).change();
-        } else {
-            $rangeSumSlider.val(this.value).change();
-        }
+        $rangeSumImport[0].value = this.value;
+        getSum = $('#range-sum-input').val();
+        getTime = $('#range-time-input').val();
 
+        if(getSum != 0 && getTime != 0 ) {
+            resultSum = getSum / 100 * coeff;
+                resultCalc = getSum / 100 * (coeff / 30);
+                $('.result-box .result-sum span').text(resultSum.toFixed(0)).number(true, 0);
+                $('.result-box .result span').text(resultCalc.toFixed(0)).number(true, 0);
+                $rangeSumSlider.val(getSum).change();
+        }
+        else {
+            this.value = 0;
+            $('.result-box .result-sum span').text('0');
+                $('.result-box .result span').text('0');
+                $rangeSumSlider.val(this.value).change();
+        }
     });
 
-    // range time 
-    $rangeTimeSlider
-        .rangeslider({
-            polyfill: false
-        })
-        .on('input', function() {
-            $rangeTimeImport[0].value = this.value;
-            getSum = $('#range-sum-input').val();
-            getTime = $('#range-time-input').val();
+    // range time
+    // $rangeTimeSlider
+    //     .rangeslider({
+    //         polyfill: false
+    //     })
+    //     .on('input', function() {
+    //         $rangeTimeImport[0].value = this.value;
+    //         getSum = $('#range-sum-input').val();
+    //         getTime = $('#range-time-input').val();
 
-            if(getSum != 0 && getTime != 0 ) {
-                resultCalc = getSum / getTime;
-                $('.result-box .result').text(resultCalc.toFixed(2));
-            }
-            else {
-                $('.result-box .result').text('0');
-            }
+    //         if(getSum != 0 && getTime != 0 ) {
+    //             resultCalc = getSum / getTime;
+    //             $('.result-box .result').text(resultCalc.toFixed(0));
+    //         }
+    //         else {
+    //             $('.result-box .result').text('0');
+    //         }
 
-        });
+    //     });
 
     $rangeTimeImport.on('input', function() {
         if (this.value == "") {
@@ -88,6 +114,32 @@ $( document ).ready(function() {
 
     });
 
+    var input = $('#range-sum-input');
+    var inputslider = $('#range-sum-input');
+
+
+    $('#range-sum-slider').on("input change", function() {
+        inputslider.val(this.value);
+    });
+
+    $('#range-sum-input').on("input", function() {
+        input.val(this.value);
+    });
+
+
+    // clear sum input on focus
+    var currentInputValue = $('#range-sum-input').val();
+    $('#range-sum-input').on("focus", function() {
+        currentInputValue = $(this).val();
+        $(this).val('');
+    });
+
+    $('#range-sum-input').on("blur", function() {
+        if (!this.value.length) {
+            $(this).val(currentInputValue).divide();
+        }
+    });
+
     // get data from sliders
     $('.range-sum-value-left').text($('#range-sum-slider').attr('min'));
     $('.range-sum-value-right').text($('#range-sum-slider').attr('max'));
@@ -96,38 +148,19 @@ $( document ).ready(function() {
     $('.range-time-value-right').text($('#range-time-slider').attr('max')).append(' месяцев');
 
 
-    // map
-    // Basic options for a simple Google Map
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-    var mapOptions = {
-        // How zoomed in you want the map to start at (always required)
-        zoom: 10,
+    // form payment
+    $( '.form-payment input[name="formPaymentSum"]' ).keyup(function() {
+        var result = ( $(this).val() / 96.2 ) * 100;
 
-        // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(55.6329645, 37.4416959),
-
-    };
-
-    // Get the HTML DOM element that will contain your map 
-    // We are using a div with id="map" seen below in the <body>
-    var mapElement = document.getElementById('map');
-
-    // Create the Google Map using our element and options defined above
-    var map = new google.maps.Map(mapElement, mapOptions);
-
-    // Let's also add a marker while we're at it
-    var icon = {
-	    url: "./images/icons/icon-location.svg", // url
-	    scaledSize: new google.maps.Size(50, 50), // scaled size
-	    origin: new google.maps.Point(0,0), // origin
-	    anchor: new google.maps.Point(0, 0) // anchor
-	};
-
-    var marker = new google.maps.Marker({
-        map: map,
-        position: new google.maps.LatLng(55.6329645, 37.4416959),
-        icon: icon,
-        animation: google.maps.Animation.DROP,
-        title: ''
+        $('.form-payment input[name="formPaymentSumPercent"]').val( result.toFixed(2) );
+        $('.form-payment input[name="formPaymentSumPercentDisabled"]').val( result.toFixed(2) );
     });
+    $( '.form-payment input[name="formPaymentSum"]' ).change(function() {
+        var result = ( $(this).val() / 96.2 ) * 100;
+        $('.form-wrapper-pay form input[name="formPaymentSumPercent"]').val( result.toFixed(2) );
+        $('.form-wrapper-pay form input[name="formPaymentSumPercentDisabled"]').val( result.toFixed(2) );
+    });
+
 });
+var in2put = $('#range-sum-input');
+$(in2put).divide();
