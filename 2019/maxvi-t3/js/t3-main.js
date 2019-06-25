6e80b6e727e0dde26352d867aedf5acdbf2edaff
atemiks <atemiks@gmail.com>
2019-06-26 00:33:12 +0300

    maxvi t3

---
+++ 2019/maxvi-t3/js/t3-main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	//animation
+    setTimeout(function(){
+    	AOS.init({
+    		anchorPlacement: 'top-bottom'
+    	});
+    }, 100);
+
+	// popup gallery
+	$('[data-fancybox]').fancybox({
+		buttons: [
+			'close'
+		]
+	})
+})
