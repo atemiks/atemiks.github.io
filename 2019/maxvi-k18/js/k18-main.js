6e604841b88c949184cc2de442b8ae2599545d63
atemiks <atemiks@gmail.com>
2019-09-29 21:42:57 +0300

    maxvi k18

---
+++ 2019/maxvi-k18/js/k18-main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+	//animation
+    setTimeout(function(){
+    	AOS.init({
+    		anchorPlacement: 'top-bottom'
+    	});
+    }, 100);
+
+
+	// // parallax display scene 
+ //    var c25DisplayScene = $('#c25-display-parallax').get(0);
+	// var c25DisplayParallaxInstance = new Parallax(c25DisplayScene);
+
+	// // parallax ui scene 
+	// var c25UIScene = $('#c25-ui-parallax').get(0);
+	// var c25UIParallaxInstance = new Parallax(c25UIScene);
+
+});
