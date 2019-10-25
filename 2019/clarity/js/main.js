afda8f3cd2382792ee4a92235d7fb34d60b9032f
atemiks <atemiks@gmail.com>
2019-10-25 12:13:37 +0300

    clarity

--- 2019/clarity/js/main.js
+++ 2019/clarity/js/main.js
@@ -24,4 +24,80 @@ $(document).ready(function() {
 	$(bg0).on('click', function() {
 		closeOffcanvas();
 	})
+
+	// Add smooth scrolling to all links
+	$(".link-anchor").on('click', function(event) {
+
+	    // Make sure this.hash has a value before overriding default behavior
+	    if (this.hash !== "") {
+	        // Prevent default anchor click behavior
+	        event.preventDefault();
+
+	        // Store hash
+	        var hash = this.hash;
+
+	        // Using jQuery's animate() method to add smooth page scroll
+	        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+	        $('html, body').animate({
+	            scrollTop: $(hash).offset().top
+	        }, 600, function() {
+
+	            // Add hash (#) to URL when done scrolling (default click behavior)
+	            window.location.hash = hash;
+	        });
+	    } // End if
+	});
+
+
+	// Phone mask
+    $("input[type='tel']").mask("+7 (999) 999-99-99");
+
+
+    // Sliders
+    $('.slider-portfolio').slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		dots: true,
+		fade: true
+	});
+
+
+	// Forms
+	$('.form-explain').on('click', '.form-explain-toggle', function (event) {
+		var target = $(event.currentTarget),
+			sectionContainer = target.closest('section'),
+			formContainer = target.closest('.form-explain'),
+			formHiddenEl = formContainer.find('.form-group.d-none');
+
+		sectionContainer.addClass('section-explained');
+		target.addClass('d-none');
+		formHiddenEl.removeClass('d-none');
+	});
+
+	$("#formModalCallback").submit(function() {
+		$.ajax({
+			type: "POST",
+			url: "/formModalCallback.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find("input").val("");
+			alert("Спасибо за заявку! Скоро мы с вами свяжемся.");
+			$("#formModalCallback").trigger("reset");
+		});
+		return false;
+	});
+
+
+	// Portfolio
+	$('.portfolio-card').each(function() {
+		var card = $(this),
+			cardTitle = $(card).find('.case-title');
+
+		if (cardTitle.text().length > 14) {
+			cardTitle.addClass('title-sm');
+		}
+	})
+
 })
