55a0966de47eb0e0dddf3b36ce3007fc00b93dbf
atemiks <atemiks@gmail.com>
2019-11-06 14:16:10 +0200

    maxvi t8 animation

--- 2019/maxvi-t8-animation/js/t8-main.js
+++ 2019/maxvi-t8-animation/js/t8-main.js
@@ -23,15 +23,24 @@ $(document).ready(function() {
 	$(window).resize(function(){
 		width = $(document).width();
 	});
+	var animOffset = 0
+		if ( $('.t8-glass').height() <= $( window ).height() ) {
+			$('.t8-glass').css('min-height', '100vh');
+		} else {
+			animOffset = 250;
+		}
 		if (width > 1199){
 			tl.to('.t8-phone-screen-block', 1, {x: -200, y:-70});
-			tl.to('.t8-phone-screen', .1, {display:'none'});
+			tl.to('.t8-phone-screen', 1, {display: 'none'});
 			tl.to('.t8-phone-screen-2', .1, {display:'block'});
 			tl.to('.t8-phone-screen-2', .3, {display:'none'});
 			tl.to('.t8-phone-screen-3', .1, {display:'block'});
 			tl.to('.t8-phone-screen-3', .3, {display:'none'});
 			tl.to('.t8-phone-screen-4', .1, {display:'block'});
 			tl.to('.t8-phone-screen-text', .5, {x:0});
+			tl.to('.t8-phone-screen-arrow-right-overvlow img', .5, {x:0}, '=-.5');
+			tl.to('.t8-phone-screen-arrow-right-overvlow span', .5, {x:0}, '=-.5');
+			tl.to('.t8-phone-screen-arrow-left-overvlow img', .5, {x:0}, '=-.5');
 			tl.to('.t8-phone-middle-block', 1, {x: 200}, "=.1");
 			tl.to('.t8-phone-back-block', 1, {x: -50, scale: .7}, "=-1");
 			tl.to('.t8-phone-back', 1, {filter: "blur(8px)"}, "=-.8");
@@ -44,7 +53,7 @@ $(document).ready(function() {
 			const scene = new ScrollMagic.Scene({
 				triggerElement: ".t8-glass",
 									triggerHook: "onLeave",
-									offset: 250,
+									offset: animOffset,
 									duration: "100%"
 			})
 				.setPin(".t8-glass")
@@ -59,6 +68,9 @@ $(document).ready(function() {
 			tl.to('.t8-phone-screen-3', .3, {display:'none'});
 			tl.to('.t8-phone-screen-4', .1, {display:'block'});
 			tl.to('.t8-phone-screen-text', .5, {x:0});
+			tl.to('.t8-phone-screen-arrow-right-overvlow img', .5, {x:0}, '=-.5');
+			tl.to('.t8-phone-screen-arrow-right-overvlow span', .5, {x:0}, '=-.5');
+			tl.to('.t8-phone-screen-arrow-left-overvlow img', .5, {x:0}, '=-.5');
 			tl.to('.t8-phone-middle-block', 1, {x: 150}, "=.1");
 			tl.to('.t8-phone-back-block', 1, {x: -50, scale: .7}, "=-1");
 			tl.to('.t8-phone-back', 1, {filter: "blur(8px)"}, "=-.8");
@@ -86,6 +98,9 @@ $(document).ready(function() {
 			tl.to('.t8-phone-screen-3', .3, {display:'none'});
 			tl.to('.t8-phone-screen-4', .1, {display:'block'});
 			tl.to('.t8-phone-screen-text', .5, {x:0});
+			tl.to('.t8-phone-screen-arrow-right-overvlow img', .5, {x:0}, '=-.5');
+			tl.to('.t8-phone-screen-arrow-right-overvlow span', .5, {x:0}, '=-.5');
+			tl.to('.t8-phone-screen-arrow-left-overvlow img', .5, {x:0}, '=-.5');
 			tl.to('.t8-phone-middle-block', 1, {x: 150}, "=.1");
 			tl.to('.t8-phone-back-block', 1, {x: -50, scale: .7}, "=-1");
 			tl.to('.t8-phone-back', 1, {filter: "blur(8px)"}, "=-.8");
@@ -98,13 +113,14 @@ $(document).ready(function() {
 			const scene = new ScrollMagic.Scene({
 				triggerElement: ".t8-glass",
 									triggerHook: "onLeave",
-									offset: 60,
+									offset: 0,
 									duration: "100%"
 			})
 				.setPin(".t8-glass")
 				.setTween(tl)
 					.addTo(controller);
 		} else if (width <= 767) {
+			$('.t8-glass').css('height', 'auto');
 			tl.to('.t8-phone-animation-block', 1, {y: 50});
 			tl.to('.t8-phone-screen-block', 1, {x: 70, y:-150}, '=-1');
 			tl.to('.t8-phone-screen', .1, {display:'none'});
@@ -114,6 +130,12 @@ $(document).ready(function() {
 			tl.to('.t8-phone-screen-3', .3, {display:'none'});
 			tl.to('.t8-phone-screen-4', .1, {display:'block'});
 			tl.to('.t8-phone-screen-text', .5, {x:0});
+			tl.to('.t8-phone-screen-text-overflow', .5, {y: '-30px'});
+			tl.to('.t8-phone-screen-arrow-right-overvlow img', .5, {x:0}, '=-.5');
+			tl.to('.t8-phone-screen-arrow-right-overvlow span', .5, {x:0}, '=-.5');
+			tl.to('.t8-phone-screen-arrow-left-overvlow img', .5, {x:0}, '=-.5');
+			tl.to('.t8-glass .t8-phone-screen-arrow-right-overvlow', .5, {y:'-70px'}, '=-.5');
+			tl.to('.t8-phone-screen-arrow-left-overvlow', .5, {y:'-68px'}, '=-.5');
 			tl.to('.t8-phone-middle-block', 1, {x: 90}, "=.1");
 			tl.to('.t8-phone-back-block', 1, {x: -20, y: -30, scale: .7}, "=-1");
 			tl.to('.t8-phone-back', 1, {filter: "blur(8px)"}, "=-.8");
@@ -126,7 +148,7 @@ $(document).ready(function() {
 			const scene = new ScrollMagic.Scene({
 				triggerElement: ".t8-glass",
 									triggerHook: "onLeave",
-									offset: 50,
+									offset: 0,
 									duration: "100%"
 			})
 				.setPin(".t8-glass")
