516957b81c2bf644cd263321918e2fe8d4dda832
atemiks <atemiks@gmail.com>
2020-01-22 07:07:52 +0200

    remont legko

--- 2020/remont-legko/formModalCalc.php
+++ 2020/remont-legko/formModalCalc.php
@@ -6,8 +6,8 @@ $headers = "From: mrakopotnik@mail.ru";
 $recepient = "mrakopotnik@mail.ru";
 $sitename = "Ремонт Легко";
 
-$name = trim($_POST["formModalCallbackName"]);
-$phone = trim($_POST["formModalCallbackPhone"]);
+$name = trim($_POST["formModalCalcName"]);
+$phone = trim($_POST["formModalCalcPhone"]);
 $message = "Имя: $name \nТелефон: $phone \n";
 
 $pagetitle = "Новая заявка с сайта \"$sitename\"";
