0d734033a58b695034fd7b9f66a9a0a2d31837dd
atemiks <atemiks@gmail.com>
2020-05-05 15:33:44 +0300

    business group auto

--- 2020/business-group-auto/js/calc.js
+++ 2020/business-group-auto/js/calc.js
@@ -1,158 +1,150 @@
-var in2put = $('#range-sum-input');
-$(in2put).divide();
-
 $(document).ready(function () {
-    // calculator
-    $.fn.setCursorPosition = function (pos) {
-        this.each(function (index, elem) {
-            if (elem.setSelectionRange) {
-                elem.setSelectionRange(pos, pos);
-            } else if (elem.createTextRange) {
-                var range = elem.createTextRange();
-                range.collapse(true);
-                range.moveEnd('character', pos);
-                range.moveStart('character', pos);
-                range.select();
+    if ($('.section-calc').length == true) {
+        $.fn.setCursorPosition = function (pos) {
+            this.each(function (index, elem) {
+                if (elem.setSelectionRange) {
+                    elem.setSelectionRange(pos, pos);
+                } else if (elem.createTextRange) {
+                    var range = elem.createTextRange();
+                    range.collapse(true);
+                    range.moveEnd('character', pos);
+                    range.moveStart('character', pos);
+                    range.select();
+                }
+            });
+            return this;
+        };
+
+        let $calcContainer = $('.section-calc'),
+            $calcPaymentMonth = $calcContainer.find('#calc-payment-month'),
+            $calcPaymentTotal = $calcContainer.find('#calc-payment-total'),
+
+            $rangeSumSlider = $('#range-sum-slider'),
+            $rangeSumInput = $('#range-sum-input'),
+
+            $rangeTimeSlider = $('#range-time-slider'),
+            $rangeTimeInput = $('#range-time-input'),
+
+            calcPersent = parseFloat($calcContainer.find('#calc-percent').text()), // persent for month
+            roubleString = ' <span class="rouble" >руб</span>';
+
+
+        function annualCalc() {
+            let tempAnnual = $rangeSumInput.val() * ((calcPersent / 100) / (1 - Math.pow((1 + (calcPersent / 100)), -$rangeTimeInput.val())));
+            return tempAnnual.toFixed(0);
+        }
+
+        function calcUpdate() {
+            if ($rangeSumInput.val() != 0 && $rangeTimeInput.val() != 0) {
+                let tempPaymentMonth = annualCalc(),
+                    tempPaymentTotal = $rangeTimeInput.val() * tempPaymentMonth;
+
+                $calcPaymentMonth.html(tempPaymentMonth).divide().append(roubleString);
+                $calcPaymentTotal.html(tempPaymentTotal).divide().append(roubleString);
+            } else {
+                $calcPaymentMonth.html('0').append(roubleString);
+                $calcPaymentTotal.html('0').append(roubleString);
+            }
+        }
+
+        function calcInit() {
+            // init sum divide
+            $rangeSumInput.divide();
+
+            // get data from sliders
+            $calcContainer.find('.range-sum-value-left').html($($rangeSumSlider).attr('min')).divide().append(' <span class="rouble">руб.</span>');
+            $calcContainer.find('.range-sum-value-right').html($($rangeSumSlider).attr('max')).divide().append(' <span class="rouble">руб.</span>');
+
+            $calcContainer.find('.range-time-value-left').html($($rangeTimeSlider).attr('min')).append(' мес.');
+            $calcContainer.find('.range-time-value-right').html($($rangeTimeSlider).attr('max')).append(' мес.');
+
+            calcUpdate();
+        }
+
+        calcInit();
+
+        // range sum
+        $rangeSumSlider
+            .rangeslider({
+                polyfill: false
+            })
+            .on('input', function () {
+                $rangeSumInput[0].value = this.value;
+
+                if (this.value > 0) {
+                    calcUpdate();
+                } else {
+                    $calcPaymentMonth.html(this.value).divide().append(roubleString);
+                    $calcPaymentTotal.html(this.value).divide().append(roubleString);
+                }
+            });
+
+        $rangeSumInput.on('input', function () {
+            $rangeSumInput[0].value = this.value;
+            calcUpdate();
+        });
+
+        // range time
+        $rangeTimeSlider
+            .rangeslider({
+                polyfill: false
+            })
+            .on('input', function () {
+                $rangeTimeInput[0].value = this.value;
+
+                calcUpdate();
+            });
+
+        $rangeTimeInput.on('input', function () {
+            if (this.value == "") {
+                this.value = 0;
+                $rangeTimeSlider.val(this.value).change();
+            } else {
+                $rangeTimeSlider.val(this.value).change();
             }
         });
-        return this;
-    };
 
-    var $rangeSumSlider = $('#range-sum-slider');
-    var $rangeSumImport = $('#range-sum-input');
 
-    var $rangeTimeSlider = $('#range-time-slider');
-    var $rangeTimeImport = $('#range-time-input');
+        $($rangeSumSlider).on("input change", function () {
+            $rangeSumInput.val(this.value);
+        });
 
-    var getSum = $('#range-sum-input').val();
-    var getTime = $('#range-time-input').val();
-    var resultSum = getSum / getTime * currentPersent;
+        $($rangeSumInput).on("input", function () {
+            $rangeSumInput.val(this.value);
+        });
 
-    var currentPersent = 0.02;
+        $($rangeTimeSlider).on("input change", function () {
+            $rangeTimeInput.val(this.value);
+        });
 
-    function calcTotalSum() {
-        var monthSum = +getSum,
-            totalSum = monthSum;
+        $($rangeTimeInput).on("input", function () {
+            $rangeTimeInput.val(this.value);
+        });
 
-        for (var i = 1; i <= getTime; i++) {
-            totalSum += monthSum + monthSum * 0.02;
-            console.log(i + ' ' + totalSum);
-        }
+
+        // clear inputs on focus
+        var currentInputValue;
+
+        $($rangeSumInput).on("focus", function () {
+            currentInputValue = $(this).val();
+            $(this).val('');
+        });
+
+        $($rangeSumInput).on("blur", function () {
+            if (!this.value.length) {
+                $(this).val(currentInputValue).divide();
+            }
+        });
+
+        $($rangeTimeInput).on("focus", function () {
+            currentInputValue = $(this).val();
+            $(this).val('');
+        });
+
+        $($rangeTimeInput).on("blur", function () {
+            if (!this.value.length) {
+                $(this).val(currentInputValue).divide();
+            }
+        });
     }
-
-    calcTotalSum();
-
-    // $('.calc-type-list').on('click', '.calc-type-item', function() {
-    //     var self = $(this);
-    //     var parrent = $(self).closest('.calc-type-list');
-
-    //     $(parrent).find('.calc-type-item').removeClass('active');
-    //     $(self).addClass('active');
-
-    //     resultSum = getSum / getTime * currentPersent ;
-    //     $('.section-calc .calc-payment-month span').text(resultSum.toFixed(0)).divide();
-    // });
-
-    resultSum = getSum / getTime * currentPersent;
-    $('.section-calc .calc-payment-month span').text(resultSum.toFixed(0)).divide();
-
-    // range sum
-    $rangeSumSlider
-        .rangeslider({
-            polyfill: false
-        })
-        .on('input', function () {
-            $rangeSumImport[0].value = this.value;
-            getSum = $('#range-sum-input').val();
-            getTime = $('#range-time-input').val();
-
-            if (getSum != 0 && getTime != 0) {
-                resultSum = getSum / getTime * currentPersent;
-                $('.section-calc .calc-payment-month span').text(resultSum.toFixed(0)).divide();
-            }
-            else {
-                $('.section-calc .calc-payment-month span').text('0');
-            }
-        });
-
-    $rangeSumImport.on('input', function () {
-        $rangeSumImport[0].value = this.value;
-        getSum = $('#range-sum-input').val();
-        getTime = $('#range-time-input').val();
-
-        if (getSum != 0 && getTime != 0) {
-            resultCalc = getSum / getTime * currentPersent;
-            $('.section-calc .calc-payment-month span').text(resultSum.toFixed(0)).divide();
-            $rangeSumSlider.val(getSum).change();
-        }
-        else {
-            this.value = 0;
-            $('.section-calc .calc-payment-month span').text('0');
-            $rangeSumSlider.val(this.value).change();
-        }
-    });
-
-    // range time
-    $rangeTimeSlider
-        .rangeslider({
-            polyfill: false
-        })
-        .on('input', function () {
-            $rangeTimeImport[0].value = this.value;
-            getSum = $('#range-sum-input').val();
-            getTime = $('#range-time-input').val();
-
-            if (getSum != 0 && getTime != 0) {
-                resultSum = getSum / getTime * currentPersent;
-                $('.section-calc .calc-payment-month span').text(resultSum.toFixed(0)).divide();
-            }
-            else {
-                $('.section-calc .calc-payment-month span').text('0');
-            }
-
-        });
-
-    $rangeTimeImport.on('input', function () {
-        if (this.value == "") {
-            this.value = 0;
-            $rangeTimeSlider.val(this.value).change();
-        } else {
-            $rangeTimeSlider.val(this.value).change();
-        }
-
-    });
-
-    var input = $('#range-sum-input');
-    var inputslider = $('#range-sum-input');
-
-
-    $('#range-sum-slider').on("input change", function () {
-        inputslider.val(this.value);
-    });
-
-    $('#range-sum-input').on("input", function () {
-        input.val(this.value);
-    });
-
-    // clear sum input on focus
-    var currentInputValue = $('#range-sum-input').val();
-    $('#range-sum-input').on("focus", function () {
-        currentInputValue = $(this).val();
-        $(this).val('');
-    });
-
-    $('#range-sum-input').on("blur", function () {
-        if (!this.value.length) {
-            $(this).val(currentInputValue).divide();
-        }
-    });
-
-
-    // get data from sliders
-    $('.range-sum-value-left').html($('#range-sum-slider').attr('min')).divide().append(' <span class="rouble">руб.</span>');
-    $('.range-sum-value-right').html($('#range-sum-slider').attr('max')).divide().append(' <span class="rouble">руб.</span>');
-
-    $('.range-time-value-left').html($('#range-time-slider').attr('min')).append(' мес.');
-    $('.range-time-value-right').html($('#range-time-slider').attr('max')).append(' мес.');
 })
-
