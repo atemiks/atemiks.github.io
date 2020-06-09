353f3080d0731315c5fd4335eddf081903566e91
atemiks <atemiks@gmail.com>
2020-06-09 20:34:43 +0300

    maxvi e5

--- 2020/maxvi-e5/js/e5-main.js
+++ 2020/maxvi-e5/js/e5-main.js
@@ -1,3 +1,34 @@
+// animate phone
+let	windowOffset = undefined, 
+	phone = $('.js-e5-phone-figure'),
+	dock = $('.js-e5-dock-figure'),
+	phoneOffset = undefined,
+	phoneStyleOffset = undefined,
+	dockOffset = undefined,
+	dockStyleOffset = undefined;
+
+function initAnimatePhone() {
+	phoneStyleOffset = parseInt(phone.css('top')),
+	phoneOffset = phone.offset().top - phoneStyleOffset,
+	dockStyleOffset = phoneStyleOffset,
+	dockOffset = dock.offset().top - dockStyleOffset;
+}
+
+function phoneAnimation() {
+	windowOffset = $(window).scrollTop();
+
+	if(windowOffset >= phoneOffset && windowOffset <= dockOffset) {
+		phone.addClass('is-fixed');
+		phone.css('top', phoneStyleOffset);
+	} else if (windowOffset >= dockOffset) {
+		phone.removeClass('is-fixed');
+		phone.css('top', dockOffset - phoneOffset + phoneStyleOffset);
+	} else {
+		phone.removeClass('is-fixed');
+		phone.css('top', phoneStyleOffset);
+	}
+}
+
 $(document).ready(function() {
 	//animation
     setTimeout(function(){
@@ -6,4 +37,19 @@ $(document).ready(function() {
     	});
     }, 100);
 
+	// init phone animation
+	initAnimatePhone();
+	phoneAnimation();
+});
+
+$(window).on('resize', function() {
+	initAnimatePhone();
+	phoneAnimation();
+});
+
+$(window).on('scroll', function() {
+	// animate phone on tablet and pc
+	if($(window).width() >= 768) {
+		phoneAnimation();
+	}
 });
